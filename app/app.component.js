6ed8f511016c5dd1ff90d6a22d34166410ee92ff
Dariusz Franczak <[darius32@op.pl]>
2017-02-17 17:40:34 +0000

    list of articles dynamicly rendered

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './article', './article.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,29 +10,40 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var AppComponent;
+    var core_1, article_1, article_component_1;
+    var AppComponent, ARTICLES;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (article_1_1) {
+                article_1 = article_1_1;
+            },
+            function (article_component_1_1) {
+                article_component_1 = article_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
-                    this.days = ['Monday', 'Tuesday', 'Wednesday'];
-                    this.month = 'January';
+                    this.articles = ARTICLES;
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-articles',
-                        template: "\n\t\t\t\t\t\t<ul>\n\t\t\t\t\t\t\t<li *ngFor = \"#day of days\">{{ day }}</li>\n\t\t\t\t\t\t</ul>\n\t\t\t\t\t\t"
+                        directives: [article_component_1.ArticleComponent],
+                        templateUrl: "app/app.component.html"
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            ARTICLES = [
+                new article_1.Article('Bootstrap', 'http://getbootstrap.com/', 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.', 'http://getbootstrap.com/apple-touch-icon.png', 0),
+                new article_1.Article('Angular2', 'https://angular.io/', 'The latest Angular release is 2.4. Learn about the latest updates to the documentation. View the Angular change log for enhancements, fixes, and breaking changes in Angular itself.', 'https://angular.io/resources/images/logos/angular2/angular.svg', 0),
+                new article_1.Article('ReactJS', 'https://facebook.github.io/react/', 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.', 'https://cdn.auth0.com/blog/react-js/react.png', 0),
+            ];
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,28 @@
 import {Component} from 'angular2/core';
 import {NgFor} from 'angular2/common';
+import {Article} from './article';
+import {ArticleComponent} from './article.component';
 
 
 @Component({
 	selector: 'my-articles',
-	template: `
-						<ul>
-							<li *ngFor = "#day of days">{{ day }}</li>
-						</ul>
-						`
+	directives: [ArticleComponent],
+	templateUrl: `app/app.component.html`
 })
 
 export class AppComponent {
-	days: string[];
-	month: string;
 
-	constructor() {
-		this.days = ['Monday', 'Tuesday', 'Wednesday'];
-		this.month = 'January';
-	}
+	articles: Article[];
+	articles = ARTICLES;
 
 }
+
+var ARTICLES: Article[] = [
+
+	new Article('Bootstrap', 'http://getbootstrap.com/', 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.', 'http://getbootstrap.com/apple-touch-icon.png', 0),
+
+	new Article('Angular2', 'https://angular.io/', 'The latest Angular release is 2.4. Learn about the latest updates to the documentation. View the Angular change log for enhancements, fixes, and breaking changes in Angular itself.', 'https://angular.io/resources/images/logos/angular2/angular.svg', 0),
+
+	new Article('ReactJS', 'https://facebook.github.io/react/', 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.', 'https://cdn.auth0.com/blog/react-js/react.png', 0),
+
+]
